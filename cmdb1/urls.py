7b66c56daae8dd9814d7a0a82c02d6c1082a10d8
zqrainra <zqrainra@users.noreply.github.com>
2016-07-15 17:42:39 +0800

    Add files via upload

---
+++ cmdb1/urls.py
@@ -0,0 +1,30 @@
+"""cmdb1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+import assets.api_view as api_view
+from assets import urls as asset_url
+router = routers.DefaultRouter()
+router.register(r'cpus',api_view.CPUViewSet)
+router.register(r'rams',api_view.RAMViewSet)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^asset/',include(asset_url)),
+    url(r'^api/',include(router.urls))
+]
